cec21f695eaf3e2fa93e5a48d2c4c6841b6d4c6b
shakshu-r <shakshu02@gmail.com>
2026-04-15 12:22:38 +0530

    Update Message.kt

--- src/models/Message.kt
+++ src/models/Message.kt
@@ -1,9 +1,17 @@
 package models
 
+enum class MessageStatus {
+    SENT,
+    FORWARDED,
+    DELIVERED
+}
+
 data class Message(
     val id: String,
     val sender: String,
     val receiver: String,
     val content: String,
-    var ttl: Int = 5
+    var ttl: Int = 5,
+    var hops: Int = 0,
+    var status: MessageStatus = MessageStatus.SENT
 )
