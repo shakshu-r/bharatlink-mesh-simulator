e82a4494306edb1c21f12bdffdd08ed0ae93a1cf
shakshu-r <shakshu02@gmail.com>
2026-04-15 11:57:08 +0530

    Update MeshNetwork.kt

--- src/mesh/MeshNetwork.kt
+++ src/mesh/MeshNetwork.kt
@@ -14,6 +14,7 @@ class MeshNetwork {
     }
 
     fun sendMessage(sender: String, receiver: String, text: String) {
+
         val encrypted = AESUtil.encrypt(text)
 
         val message = Message(
