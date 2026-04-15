0f8f6eabe71c624c3e621cffae2361213c28a72a
shakshu-r <shakshu02@gmail.com>
2026-04-15 12:22:59 +0530

    Update MeshNode.kt

--- src/mesh/MeshNode.kt
+++ src/mesh/MeshNode.kt
@@ -2,6 +2,7 @@ package mesh
 
 import models.Device
 import models.Message
+import models.MessageStatus
 import encryption.AESUtil
 
 class MeshNode(val device: Device) {
@@ -10,28 +11,40 @@ class MeshNode(val device: Device) {
 
     fun receive(message: Message) {
 
-        // Prevent duplicate forwarding
+        // Prevent duplicate messages
         if (seenMessages.contains(message.id)) return
         seenMessages.add(message.id)
 
+        // Decrypt message
         val decrypted = AESUtil.decrypt(message.content)
+
+        println("📡 Message ${message.id} reached ${device.id} (Hops: ${message.hops})")
 
         // If message reached destination
         if (device.id == message.receiver) {
+            message.status = MessageStatus.DELIVERED
             println("📩 ${device.id} received: $decrypted")
+            println("✅ Message Delivered in ${message.hops} hops\n")
             return
         }
 
-        // Forward if TTL > 0
+        // Forward message if TTL allows
         if (message.ttl > 0) {
             message.ttl--
+            message.hops++
+            message.status = MessageStatus.FORWARDED
+
             forward(message)
+        } else {
+            println("❌ Message ${message.id} dropped (TTL expired)\n")
         }
     }
 
     private fun forward(message: Message) {
         for (neighbor in device.neighbors) {
+
             println("🔁 ${device.id} → forwarding to ${neighbor.id}")
+
             MeshNode(neighbor).receive(message)
         }
     }
