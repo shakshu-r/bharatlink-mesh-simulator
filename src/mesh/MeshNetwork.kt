fbb5177d5a92c4c1d11c2244d5685838c42b1123
shakshu-r <shakshu02@gmail.com>
2026-04-15 11:49:49 +0530

    Create MeshNetwork.kt

---
+++ src/mesh/MeshNetwork.kt
@@ -0,0 +1,30 @@
+package mesh
+
+import models.Device
+import models.Message
+import encryption.AESUtil
+import java.util.*
+
+class MeshNetwork {
+
+    private val nodes = mutableMapOf<String, MeshNode>()
+
+    fun addDevice(device: Device) {
+        nodes[device.id] = MeshNode(device)
+    }
+
+    fun sendMessage(sender: String, receiver: String, text: String) {
+        val encrypted = AESUtil.encrypt(text)
+
+        val message = Message(
+            id = UUID.randomUUID().toString(),
+            sender = sender,
+            receiver = receiver,
+            content = encrypted
+        )
+
+        println("🚀 Sending message from $sender to $receiver")
+
+        nodes[sender]?.receive(message)
+    }
+}
